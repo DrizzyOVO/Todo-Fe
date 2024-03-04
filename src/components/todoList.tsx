4835410611f1d2e4f3e92f479d8cdd9b70a32d1d
Gaurav P. Malpedi <gaurav.malpedi17@gmail.com>
2024-03-04 23:32:59 +0530

    enhancement

--- src/components/todoList.tsx
+++ src/components/todoList.tsx
@@ -33,7 +33,8 @@ const TodoList = () => {
         getTodos();
     }, []);
 
-    const addTodo = async () => {
+    const addTodo = async (event: any) => {
+        event.preventDefault(); 
         const response = await axios.post(`${process.env.REACT_APP_BASE_URL}/todo/todos`, { 
             title: title, 
         }, {
@@ -80,16 +81,18 @@ const TodoList = () => {
 
 
             <div className='flex justify-between'>
+                <form onSubmit={addTodo}>
                 <input type='text' 
-                    className='bg-gray-900 outline-none rounded-md px-2 xl:pr-20 2xl:pr-40'
+                    className='bg-gray-900 outline-none rounded-md px-2 xl:pr-20 2xl:pr-40 p-2'
                     value={title} 
                     onChange={(e) => setTitle(e.target.value)} 
                         placeholder='Title' 
                 />
+                </form>
 
                 <button 
                     onClick={addTodo}
-                    className='p-1 bg-gray-900 rounded-md px-2'
+                    className='p-1 bg-gray-900 rounded-md px-1'
                 >Add Todo</button>
             </div>
 
@@ -102,7 +105,7 @@ const TodoList = () => {
                     <div className='flex justify-between items-center mt-3' > 
                         <div className='' onClick={() => markDone(todo._id)} style={{ textDecoration: todo.done && "line-through", color: todo.done && "#9CA3AF" }}>
                             <input className="hidden" type="checkbox" id="task_6" checked />
-                            <label className="flex items-center h-full px-2 rounded cursor-pointer hover:bg-gray-900" >
+                            <label className="flex items-center h-full px-2 rounded cursor-pointer hover:bg-gray-900 hover:px-2 hover:p-0.5" >
                                 <span className="flex items-center justify-center w-5 h-5 text-transparent border-2 border-gray-500 rounded-full" style={{ backgroundColor: todo.done && "#10B981", borderColor: todo.done && "#10B981", color: todo.done && "#9CA3AF" }}>
                                     <svg className="w-4 h-4 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                                         <path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd" />
