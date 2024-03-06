8624e6e16ae37d6cc2062ba6301a4b5efd7acc6b
Gaurav Malpedi <99645553+DrizzyOVO@users.noreply.github.com>
2024-03-06 11:15:09 +0530

    Update font size

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -16,18 +16,18 @@ export const Landing = () => {
     } else { 
         return (
             <>
-            <h1 className="flex justify-center font-normal cursor-pointer mt-10 text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl" style={{fontFamily: 'Cookie', color: 'white'}}> 
+            <h1 className="flex justify-center font-normal cursor-pointer mt-10 text-2xl sm:text-3xl md:text-4xl lg:text-4xl xl:text-4xl" style={{fontFamily: 'Cookie', color: 'white'}}> 
                 Welcome to <span className="font-bold">&nbsp;ToDo...</span>
             </h1>
-            <h1 className="flex justify-center font-normal cursor-pointer mt-10 text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl" style={{fontFamily: 'Cookie', color: 'white'}}> 
+            <h1 className="flex justify-center font-normal cursor-pointer mt-10 text-2xl sm:text-3xl md:text-4xl lg:text-4xl xl:text-4xl" style={{fontFamily: 'Cookie', color: 'white'}}> 
                 login or make an account and get <span className="font-bold">&nbsp;started!</span>
             </h1>
 
             <div className="absolute inset-x-0 bottom-0">
-                <h1 className="flex justify-center font-normal cursor-pointer mt-10 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl" style={{fontFamily: 'Cookie', color: 'white'}}> 
+                <h1 className="flex justify-center font-normal cursor-pointer mt-10 text-xl sm:text-xl md:text-3xl lg:text-3xl xl:text-3xl" style={{fontFamily: 'Cookie', color: 'white'}}> 
                     Limited to <span className="font-bold">&nbsp;35</span>&nbsp;users.
                 </h1>
-                <h1 className="flex justify-center font-normal cursor-pointer mt-10 text-xl sm:text-2xl md:text-3xl lg:text-4xl xl:text-5xl" style={{fontFamily: 'Cookie', color: 'white'}}> 
+                <h1 className="flex justify-center font-normal cursor-pointer mt-10 text-xl sm:text-xl md:text-3xl lg:text-3xl xl:text-3xl" style={{fontFamily: 'Cookie', color: 'white'}}> 
                     Once limit is reached you may need to signup once again.
                 </h1>
             </div>
